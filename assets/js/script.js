c65a8dbd596e20722e85c6bad55acb9e5a4b443b
Christine Zheng <christinezheng@Meltons-MacBook-Pro.local>
2022-01-11 15:38:56 -0800

    changes to the task infos" q "
    ...
     ...
    "
    ""

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,32 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var createTaskHandler = function (event) {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']");
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    console.log(taskNameInput);
+
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
+    // create div
 
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
 
+    
+    taskInfoEl.innerHTML = "<h3 class-'task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // listItemEl.textContent = taskNameInput;
+    tasksToDoEl.appendChild(listItemEl);
+
+    console.dir(listItemEl);
 }
 formEl.addEventListener("submit", createTaskHandler);
 
